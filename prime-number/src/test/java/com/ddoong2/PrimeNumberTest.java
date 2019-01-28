a3b533709816d60e5b425cf3276608cf04499420
daejoon <kkode1911@gmail.com>
2019-01-28 17:09:40 +0900

    Add Library org.assertj.assertj-core

--- prime-number/src/test/java/com/ddoong2/PrimeNumberTest.java
+++ prime-number/src/test/java/com/ddoong2/PrimeNumberTest.java
@@ -3,9 +3,7 @@ package com.ddoong2;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrimeNumberTest {
 
@@ -25,46 +23,47 @@ public class PrimeNumberTest {
         } catch (Exception e) {
             thrown = e;
         }
-        assertThat(thrown, instanceOf(IllegalArgumentException.class));
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void test_2를_입력하면_2인_배열을_반환한다() {
         int[] result = primeNumber.process(2);
-        assertArrayEquals(new int[] {2}, result);
+
+        assertThat(new int[]{2}).containsSequence(result);
     }
 
     @Test
     public void test_3을_입력하면_2와_3인_배열을_반환한다() {
         int[] result = primeNumber.process(3);
-        assertArrayEquals(new int[] {2, 3}, result);
+        assertThat(new int[] {2, 3}).containsSequence(result);
     }
 
     @Test
     public void test_입력값에_해당하는_소인수분해를_반환한다() {
         int[] result = primeNumber.process(4);
-        assertArrayEquals(new int[] {2, 3}, result);
+        assertThat(new int[] {2, 3}).containsSequence(result);
 
         result = primeNumber.process(5);
-        assertArrayEquals(new int[] {2, 3, 5}, result);
+        assertThat(new int[] {2, 3, 5}).containsSequence(result);
 
         result = primeNumber.process(6);
-        assertArrayEquals(new int[] {2, 3, 5}, result);
+        assertThat(new int[] {2, 3, 5}).containsSequence(result);
 
         result = primeNumber.process(7);
-        assertArrayEquals(new int[] {2, 3, 5, 7}, result);
+        assertThat(new int[] {2, 3, 5, 7}).containsSequence(result);
 
         result = primeNumber.process(8);
-        assertArrayEquals(new int[] {2, 3, 5, 7}, result);
+        assertThat(new int[] {2, 3, 5, 7}).containsSequence(result);
 
         result = primeNumber.process(9);
-        assertArrayEquals(new int[] {2, 3, 5, 7}, result);
+        assertThat(new int[] {2, 3, 5, 7}).containsSequence(result);
 
         result = primeNumber.process(10);
-        assertArrayEquals(new int[] {2, 3, 5, 7}, result);
+        assertThat(new int[] {2, 3, 5, 7}).containsSequence(result);
 
         result = primeNumber.process(11);
-        assertArrayEquals(new int[] {2, 3, 5, 7, 11}, result);
+        assertThat(new int[] {2, 3, 5, 7, 11}).contains(result);
     }
 
 }
