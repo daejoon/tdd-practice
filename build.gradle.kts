93dd097d9068e33702dd22a73adfcdcec3f5ae76
daejoon <kkode1911@gmail.com>
2023-09-03 15:08:08 +0900

    Upgrade Project
    - Update Junit from 4 to 5
    - Added sdkman
    - Using JDK 17.0
    - Changed Gradle DSL Language from groovy to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+    ext["junitVersion"] = "5.10.0"
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    group = "com.ddoong2"
+    version = "1.0.0"
+
+    java.sourceCompatibility = JavaVersion.VERSION_17
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:${project.ext["junitVersion"]}"))
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher") {
+            because("Only needed to run tests in a version of IntelliJ IDEA that bundles older versions")
+        }
+        testImplementation("org.junit.jupiter:junit-jupiter-engine")
+        testImplementation("org.junit.vintage:junit-vintage-engine")
+        testImplementation("org.assertj:assertj-core:3.24.2")
+        testImplementation("org.mockito:mockito-core:4.8.1")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "tdd-practice"
+
+include(":study:authservice")
+include(":study:bank-business")
+include(":study:bank-kata")
+include(":study:baseball-game")
+include(":study:bowling")
+include(":study:closer-to-zero")
+include(":study:lets-play-tdd")
+include(":study:movie")
+include(":study:name-inverter")
+include(":study:prime-number")
+include(":study:string-calculator")
+include(":study:test-driven-development-by-example")
+include(":study:leap-year")
+
